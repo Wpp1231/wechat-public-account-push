a50496859bdfb57f6ec40ad45e198f95c206d0cf
Wpp1231 <112362826+Wpp1231@users.noreply.github.com>
2022-08-29 10:37:41 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxfec3f5e93001b602",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "e17690d536866104d5b32766565e9c69",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: " 	r_V1BVNP_H9CyfjJyaImfXON4CQ3_9X4hIAxNvTBteE",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["wx1", "wx2"],
      
     /**
      * 信息配置
@@ -19,16 +19,16 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "陕西",
     // 所在城市
-    CITY: "惠州",
+    CITY: "西安",
 
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
     BIRTHDAYS: [
-      {"name": "老婆", "year": "1993", "date": "12-27"},
-      {"name": "家公", "year": "1993", "date": "08-09"},
+      {"name": "老婆", "year": "2012", "date": "9-1"},
+      {"name": "家公", "year": "2012", "date": "9-09"},
     ],
 
     /** 日期相关 */
@@ -43,7 +43,7 @@ export const config = {
 
     // 好文节选的内容类型
     // 可以填写【动画，漫画，游戏，小说，原创，网络，其他】； 随机则填写 ""
-    LITERARY_PREFERENCE: ""
+    LITERARY_PREFERENCE: " 有幸相遇,不负遇见"
 
 
     }
