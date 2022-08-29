21a57bad395cbdda32cddb97a2336d37ca769e28
Wpp1231 <112362826+Wpp1231@users.noreply.github.com>
2022-08-29 21:22:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,11 +4,11 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "wxfec3f5e93001b602",
+    APP_ID: "wxa8cf17777512385b",
     // 公众号APP_SECRET
-    APP_SECRET: "e17690d536866104d5b32766565e9c69",
+    APP_SECRET: "01d347a651d345029a0be0034b9a25d6",
     // 模板消息id
-    TEMPLATE_ID: " 	r_V1BVNP_H9CyfjJyaImfXON4CQ3_9X4hIAxNvTBteE",
+    TEMPLATE_ID: "4-W1N7uskM4SFc2kUojYU8FfCDGw0Fg_cw0F5bn3gAs",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     USERS: ["Vir-0911-W"],
      
@@ -26,17 +26,17 @@ export const config = {
     /** 生日相关 */
 
     // 生日，修改名字为对应需要显示的名字, data 仅填月日即可, 请严格按照示例填写
-    BIRTHDAYS: [
-      {"name": "老婆", "year": "2012", "date": "9-1"},
-      {"name": "家公", "year": "2012", "date": "9-09"},
-    ],
+   // BIRTHDAYS: [
+     // {"name": "老婆", "year": "2012", "date": "9-1"},
+    //  {"name": "家公", "year": "2012", "date": "9-09"},
+   // ],
 
     /** 日期相关 */
 
     // 在一起的日子，格式同上
-    LOVE_DATE: "2015-05-01",
+   // LOVE_DATE: "2015-05-01",
     // 结婚纪念日
-    MARRY_DATE: "2020-01-04",
+    //MARRY_DATE: "2020-01-04",
 
 
     /** 好文节选 */
